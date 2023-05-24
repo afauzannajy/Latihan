15aca5a606431d76ba646c8764d692c4291b102d
afauzannajy <zannajy212@gmail.com>
2023-05-24 15:13:06 +0700

    Menambahkan 2 fungsi pada javascript omdb api -> js/script.js

--- 2. Javascript OMDB API/js/script.js
+++ 2. Javascript OMDB API/js/script.js
@@ -6,17 +6,7 @@ $(".search-button").on("click", function () {
       const movies = results.Search;
       let card = "";
       movies.forEach((m) => {
-        card += `<div class="col-md-3 my-5">
-                  <div class="card">
-                    <img src="${m.Poster}" class="card-img-top" />
-                    <div class="card-body">
-                      <h5 class="card-title">${m.Title}</h5>
-                      <p class="card-text">${m.Year}</p>
-                      <a href="#" class="btn btn-success modal-detail-button" data-bs-toggle="modal"
-                        data-bs-target="#movieDetailModal" data-imdbid = "${m.imdbID}">Show Details</a>
-                    </div>
-                  </div>
-                </div>`;
+        card += showMovies(m);
       });
       $(".movie-container").html(card);
       $(".modal-detail-button").on("click", function () {
@@ -26,23 +16,7 @@ $(".search-button").on("click", function () {
             "http://www.omdbapi.com/?apikey=2b1184cb&i=" +
             $(this).data("imdbid"),
           success: (md) => {
-            const movieDetail = `<div class="container-fluid">
-                                  <div class="row">
-                                      <div class="col-md-3">
-                                      <img src="${md.Poster}" class="img-fluid" />
-                                      </div>
-                                      <div class="col-md">
-                                      <ul class="list-group" style="text-align: justify;">
-                                          <li class="list-group-item"><h4>${md.Title} (${md.Year})</h4></li>
-                                          <li class="list-group-item"><strong>Genre : </strong>${md.Genre}</li>
-                                          <li class="list-group-item"><strong>Director : </strong>${md.Director}</li>
-                                          <li class="list-group-item"><strong>Writer : </strong>${md.Writer}</li>
-                                          <li class="list-group-item"><strong>Actors : </strong>${md.Actors}</li>
-                                          <li class="list-group-item"><strong>Plot : </strong>${md.Plot}</li>
-                                      </ul>
-                                      </div>
-                                  </div>
-                              </div>`;
+            const movieDetail = showMovieDetail(md);
             $(".modal-body").html(movieDetail);
           },
           error: (e) => {
@@ -56,3 +30,37 @@ $(".search-button").on("click", function () {
     },
   });
 });
+
+function showMovies(m) {
+  return `<div class="col-md-3 my-5">
+          <div class="card">
+            <img src="${m.Poster}" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">${m.Title}</h5>
+              <p class="card-text">${m.Year}</p>
+              <a href="#" class="btn btn-success modal-detail-button" data-bs-toggle="modal"
+                data-bs-target="#movieDetailModal" data-imdbid = "${m.imdbID}">Show Details</a>
+            </div>
+          </div>
+        </div>`;
+}
+
+function showMovieDetail(md) {
+  return `<div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                <img src="${md.Poster}" class="img-fluid" />
+                </div>
+                <div class="col-md">
+                <ul class="list-group" style="text-align: justify;">
+                    <li class="list-group-item"><h4>${md.Title} (${md.Year})</h4></li>
+                    <li class="list-group-item"><strong>Genre : </strong>${md.Genre}</li>
+                    <li class="list-group-item"><strong>Director : </strong>${md.Director}</li>
+                    <li class="list-group-item"><strong>Writer : </strong>${md.Writer}</li>
+                    <li class="list-group-item"><strong>Actors : </strong>${md.Actors}</li>
+                    <li class="list-group-item"><strong>Plot : </strong>${md.Plot}</li>
+                </ul>
+                </div>
+            </div>
+        </div>`;
+}
